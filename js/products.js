3e90ecfb62f8f8c1f40bac2a889eacf85bd6b2c0
Igor-01-Gomes <www.igorgomes@hotmail.com>
2026-03-20 08:07:39 +0100

    feat: add product flow and basic order page integration

--- js/products.js
+++ js/products.js
@@ -1,1 +1,22 @@
-﻿
+﻿fetch('https://fakestoreapi.com/products')
+    .then(res => res.json())
+    .then(products => {
+        const container = document.getElementById('product-container');
+
+        const product = products[0];
+
+        const div = document.createElement('div');
+
+        div.innerHTML = `
+            <h3>${product.title}</h3>
+            <button>Order</button>
+        `;
+
+        const button = div.querySelector('button');
+
+        button.addEventListener('click', () => {
+            window.location.href = 'order.html';
+        });
+
+        container.appendChild(div);
+    });
